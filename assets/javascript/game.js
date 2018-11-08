2051b63a58dc41b2245fd0897598d544debbb603
Napoleon <wanderofrazgriz@gmail.com>
2018-11-07 16:04:47 -0800

    JS updated

---
+++ assets/javascript/game.js
@@ -0,0 +1,46 @@
+
+$(document).ready(function() {
+
+    //Variables
+    //====================================
+    var choices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    var wins = 0;
+    var losses = 0;
+    var guessRemain = 10;
+    var guessCurrent = "";
+    var lettersGuessed = [];
+    var computerChoice = choices[Math.floor(Math.random() * choices.length)];
+
+    // Functions
+    //===================================
+    document.onkeyup = function (event) {
+        var guessCurrent = event.key;
+
+        if (lettersGuessed.indexOf(guessCurrent) < 0 && choices.indexOf(guessCurrent) >= 0) {
+            lettersGuessed[lettersGuessed.length] = guessCurrent;
+            guessRemain--;
+
+        }
+
+        if (computerLetter == guessCurrent) {
+            wins++;
+            // Guesses++;
+            lettersGuessed = [];
+            computerLetter = choices[Math.floor(Math.random() * choices.length)];
+            document.getElementById("Wins").textContent = + wins;
+        }
+
+        if (guessRemain == 0) {
+            losses++;
+            guessRemain = 10;
+            lettersGuessed = [];
+            computerLetter = choices[Math.floor(Math.random() * choices.length)];
+            document.getElementById("Losses").textContent = + losses;
+        }
+
+
+        document.getElementById("Guesses").textContent = + guessRemain;
+        document.getElementById("lettersGuessed").textContent = lettersGuessed;
+    }
+    console.log(computerChoice)
+});
